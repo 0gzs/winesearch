57f07d720ed769928074e350dcf00dde00ebb8b0
Jesus Rivera <r.jesus9107@gmail.com>
2023-06-07 11:38:05 -0700

    [fix] responsive css: form and header

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './dropdown.scss'
 import fieldData from '../data/fieldData.js'
 
@@ -7,6 +7,7 @@ const Dropdown = ({ onChange, title, icon, name }) => {
 
   const [opened, setOpened] = useState(false)
   const [selected, setSelected] = useState(null)
+  const [hide, setHide] = useState(false)
 
   const toggle = () => setOpened(!opened)
 
@@ -22,19 +23,34 @@ const Dropdown = ({ onChange, title, icon, name }) => {
     setSelected(option)
   }
 
+  useEffect(() => {
+    // on resize if screen size is less than 400 set hide to true
+    const handleResize = () => {
+      if (window.innerWidth < 450) setHide(true)
+      else setHide(false)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div className="dropdown">
-      <div style={{ position: 'relative' }}>
-        <i className={icon + " icon-left"}></i>
-        <button className='button' type="button" onClick={toggle}>
-          {selected || title}
-          {opened && !selected ? (
+    <div className="dropdown" onClick={toggle}>
+      <i className={icon + " icon-left"}></i>
+      <button className='button' type="button">
+        {hide ? '' : selected ? selected : title}
+      </button>
+      {opened && !selected ? (
             <i className="fa-solid fa-chevron-up icon-right"></i>
-          ) : !selected ?
-            (<i className='fa-solid fa-chevron-down icon-right'></i>
-            ) : (<i className='fa-solid fa-xmark icon-right exit' onClick={reset}></i>)}
-        </button>
-      </div>
+          ) : !selected ? (
+            <i className='fa-solid fa-chevron-down icon-right'></i>
+          ) : (
+            <i className='fa-solid fa-xmark icon-right exit' onClick={reset}></i>
+          )
+        }
       {opened && (
         <div className="items-container" onMouseLeave={() => {
           if (opened) setOpened(false)
