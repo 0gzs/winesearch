210b51ffd89c89795d8e911299535aa5aaede5a0
Jesus <37415797+jesusriv@users.noreply.github.com>
2024-01-13 23:01:54 -0800

    Refactor wine card css and grid layout

--- src/components/wine.jsx
+++ src/components/wine.jsx
@@ -8,26 +8,47 @@ const Wine = ({ wine, keywords }) => {
 
   return (
     <div className='card'>
-      <div className='product-img'>
-        <img src={wine.image} alt={wine.name} />
-      </div>
-      <h3>
-        <Highlighter
-          highlightClassName='highlight'
-          searchWords={keywords}
-          autoEscape={true}
-          textToHighlight={wine.name}
-        />
-      </h3>
+        <div className='product-img'>
+          <img src={wine.image} alt={wine.name} />
+        </div>
+          <h3>
+            <Highlighter
+              highlightClassName='highlight'
+              searchWords={keywords}
+              autoEscape={true}
+              textToHighlight={wine.name}
+            />
+          </h3>
+
+          <p className='product-description'>
+            <Highlighter
+              highlightClassName='highlight'
+              searchWords={keywords}
+              autoEscape={true}
+              textToHighlight={wine.description}
+            />
+          </p>
       <div className='product-details'>
-        <p className='product-description'>
-          <Highlighter
-            highlightClassName='highlight'
-            searchWords={keywords}
-            autoEscape={true}
-            textToHighlight={wine.description}
-          />
-        </p>
+        <ul>
+          <span>
+            <strong>Price:</strong> {wine.price}
+          </span>
+          <span>
+            <strong>Base Price:</strong> ${wine.base_price}
+          </span>
+          <span>
+            <strong>Type:</strong> {wine.type}
+          </span>
+          <span>
+            <strong>Brand:</strong> {wine.brand}
+          </span>
+          <span>
+            <strong>Size:</strong> {wine.size}
+          </span>
+          <span>
+            <strong>SKU:</strong> {wine.sku}
+          </span>
+        </ul>
       </div>
       {barcode ? (
         <div className="barcode">
